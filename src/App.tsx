8deeb74b332ad647fc0c933009ad68e70490cbb7
Marek Supruniuk <marek.supruniuk@gmail.com>
2022-03-05 18:06:22 +0100

    Add emotion support

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { Text } from './styles';
+
 function App() {
   return (
     <div className="App">
@@ -10,6 +12,7 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <Text>test of styled component</Text>
         <a
           className="App-link"
           href="https://reactjs.org"
